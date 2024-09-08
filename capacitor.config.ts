bc88ee426867cadfe91daab725fd7d257ec827af
KennethLicuanan <licuanankennethlloyd05@gmail.com>
2024-09-08 12:32:09 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'capstone_admin',
+  webDir: 'dist'
+};
+
+export default config;
