d0fda207545b2c5d95c44a685ba9dde10aea733b
KennethLicuanan <licuanankennethlloyd05@gmail.com>
2024-09-23 00:37:59 +0800

    Updates

--- assets/md.transition-legacy-NzDx8hO-.js
+++ assets/md.transition-legacy-NzDx8hO-.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CIp5QK_b.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DpSmnYSD.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
